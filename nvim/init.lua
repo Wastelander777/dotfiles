5f82d5bae24eee68dbe17d7326a7cd2d3e586895
Wastlander777 <ugarte.p711@gmail.com>
2024-11-28 17:11:31 +0100

    nvim dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,83 @@
+-- Install 'mini.nvim' if not already present
+local path_package = vim.fn.stdpath("data") .. "/site"
+local mini_path = path_package .. "/pack/deps/start/mini.nvim"
+
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    -- Uncomment next line to use the 'stable' branch of mini.nvim
+    -- '--branch', 'stable',
+    "https://github.com/echasnovski/mini.nvim",
+    mini_path,
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd("packadd mini.nvim | helptags ALL")
+end
+
+-- Set global variables for cache path and leader key
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
+vim.g.mapleader = " " -- Use space as the leader key
+
+-- Bootstrap 'lazy.nvim' and load plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Load 'lazy.nvim' configuration
+local lazy_config = require("configs.lazy")
+
+-- Setup 'lazy.nvim' with plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false, -- Load NvChad immediately
+    branch = "v2.5", -- Specify branch version
+    import = "nvchad.plugins", -- Import NvChad plugin settings
+  },
+  { import = "plugins" }, -- Import additional plugins defined in 'plugins.lua'
+}, lazy_config)
+
+-- Load the theme settings
+dofile(vim.g.base46_cache .. "defaults")   -- Load theme defaults
+dofile(vim.g.base46_cache .. "statusline") -- Load statusline configuration
+
+-- Load Neovim options and autocommands
+require("options")
+require("nvchad.autocmds")
+
+-- Schedule mappings after other components have loaded
+vim.schedule(function()
+  require("mappings")
+end)
+
+-- Restore cursor to last edited position when reopening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Open file at the last position it was edited earlier",
+  group = misc_augroup,
+  pattern = "*",
+  command = 'silent! normal! g`"zv',
+})
+
+-- Setup 'nvim-treesitter' with automatic parser installation
+require("nvim-treesitter.configs").setup({
+  -- List of parsers to ensure are always installed
+  ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "yaml" },
+  sync_install = true, -- Install parsers synchronously
+  auto_install = true, -- Automatically install missing parsers
+  sync_root_with_cwd = true, -- Automatically refresh directory
+})
+
+-- Setup additional plugins
+require("oil").setup()          -- File explorer plugin
+-- require("mini.align").setup()   -- Text alignment plugin
+-- require('mini.ai').setup()    -- Advanced text objects (commented out for now)
+require("mini.animate").setup() -- Smooth scrolling and window transitions
+require("mini.basics").setup()  -- Basic settings like better mappings
---
+++ nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,169 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+require("lspconfig").yamlls.setup {}
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "pyright", "lua_ls", "ruff", "yamlls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+require("null-ls").setup {
+  sources = {
+    require("null-ls").builtins.formatting.black,
+    require("null-ls").builtins.formatting.isort,
+    -- require("null-ls").builtins.diagnostics.pylint,
+    require("null-ls").builtins.diagnostics.flake8,
+    require("null-ls").builtins.formatting.stylua,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { async = true }
+        end,
+      })
+    end
+  end,
+}
+
+-- Import the necessary modules
+local lspconfig = require "lspconfig"
+local util = require "lspconfig.util"
+
+-- Files to detect the root directory
+local root_files = {
+  "pyproject.toml",
+  "setup.py",
+  "setup.cfg",
+  "requirements.txt",
+  "Pipfile",
+  "pyrightconfig.json",
+  ".git",
+}
+
+-- Command to organize imports using Pyright
+local function organize_imports()
+  local params = {
+    command = "pyright.organizeimports",
+    arguments = { vim.uri_from_bufnr(0) },
+  }
+
+  local clients = vim.lsp.get_active_clients {
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = "pyright",
+  }
+  for _, client in ipairs(clients) do
+    client.request("workspace/executeCommand", params, nil, 0)
+  end
+end
+
+-- Command to set Python path in Pyright
+local function set_python_path(path)
+  local clients = vim.lsp.get_active_clients {
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = "pyright",
+  }
+  for _, client in ipairs(clients) do
+    if client.settings then
+      client.settings.python = vim.tbl_deep_extend("force", client.settings.python, { pythonPath = path })
+    else
+      client.config.settings = vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
+    end
+    client.notify("workspace/didChangeConfiguration", { settings = nil })
+  end
+end
+
+-- Register Pyright as the LSP server for Python
+lspconfig.pyright.setup {
+  cmd = { "pyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_dir = function(fname)
+    return util.root_pattern(unpack(root_files))(fname)
+  end,
+  single_file_support = true,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly",
+      },
+    },
+  },
+  commands = {
+    PyrightOrganizeImports = {
+      organize_imports,
+      description = "Organize Imports",
+    },
+    PyrightSetPythonPath = {
+      set_python_path,
+      description = "Reconfigure pyright with the provided python path",
+      nargs = 1,
+      complete = "file",
+    },
+  },
+}
+
+local util = require "lspconfig.util"
+
+local root_files = {
+  "pyproject.toml",
+  "ruff.toml",
+}
+
+return {
+  default_config = {
+    cmd = { "ruff" },
+    filetypes = { "python" },
+    root_dir = util.root_pattern(unpack(root_files)) or util.find_git_ancestor(),
+    single_file_support = true,
+    settings = {},
+  },
+  docs = {
+    description = [[
+https://github.com/astral-sh/ruff-lsp
+
+A Language Server Protocol implementation for Ruff, an extremely fast Python linter and code transformation tool, written in Rust. It can be installed via pip.
+
+```sh
+pip install ruff-lsp
+```
+
+Extra CLI arguments for `ruff` can be provided via
+
+```lua
+require'lspconfig'.ruff_lsp.setup{
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+```
+
+  ]],
+    root_dir = [[root_pattern("pyproject.toml", "ruff.toml", ".git")]],
+  },
+}
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
