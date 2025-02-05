5951e1ce16056c2b715b2ca86cae38cbf7168682
Wastelander777 <ugarte.p711@gmail.com>
2025-02-05 10:31:50 +0100

    flake8 added

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -6,7 +6,7 @@ require("lspconfig").jdtls.setup {}
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "pyright", "lua_ls", "ruff", "yamlls", "terraformls", "jdtls", "lemminx","bashls"}
+local servers = { "pyright", "lua_ls", "ruff", "yamlls", "terraformls", "jdtls", "lemminx", "bashls", "flake8" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
