6e49dca67dd6ab738ca6b1dc9f8f0360370fd4be
Wastlander777 <ugarte.p711@gmail.com>
2025-06-16 17:50:09 +0200

    curated nvim

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,18 +1,22 @@
-local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black", "isort" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
-
-  -- Configuración para formateo automático al guardar
-  format_on_save = {
-    -- Estas opciones se pasarán a conform.format()
-    timeout_ms = 500,  -- Tiempo de espera para el formateo
-    lsp_fallback = true,  -- Usa LSP como fallback si el formateador no está disponible
-  },
+-- Formatter configuration per filetype
+local formatters_by_ft = {
+  lua     = { "stylua"            }, -- Lua
+  python  = { "black", "isort"   }, -- Python
+  css     = { "prettier"         }, -- CSS
+  html    = { "prettier"         }, -- HTML
+  sh      = { "shfmt"            }, -- Shell scripts
+  yaml    = { "yamlfmt", "yamllint" }, -- YAML
+  Dockerfile = { "hadolint"      }, -- Dockerfiles
+  terraform   = { "terraform_fmt", "tflint" }, -- Terraform
 }
 
-return options
+-- Auto-format on save
+local format_on_save = {
+  timeout_ms   = 500,   -- max wait for formatting
+  lsp_fallback = true,  -- fall back to LSP if external tool missing
+}
 
+return {
+  formatters_by_ft    = formatters_by_ft,
+  format_on_save      = format_on_save,
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,20 +5,79 @@ return {
     build = ":TSUpdate",
     opts = {
       ensure_installed = { "lua", "python", "yaml", "bash", "java" },
-      highlight = { enable = true }
-    }
+      highlight = { enable = true },
+    },
   },
 
-  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
-  { "stevearc/oil.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-  -- { "folke/trouble.nvim", cmd = "Trouble", opts = {}, keys = { … } },
-  { "echasnovski/mini.nvim", version = "*" },
-  { "mfussenegger/nvim-jdtls" }, -- Java DAP helper 
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
+  {
+    "echasnovski/mini.nvim",
+    version = "*",
+  },
+
+  {
+    "mfussenegger/nvim-jdtls",
+  }, -- Java DAP helper
 
   --------------------------------------------------------------------------
   -- Everything Mason / LSP / null-ls related is pulled in *once* below
   --------------------------------------------------------------------------
-  { import = "plugins.mason" },
-  { import = "plugins.python" },
-  { import = "plugins.devops" },
+  {
+    import = "plugins.mason",
+  },
+  {
+    import = "plugins.python",
+  },
+  {
+    import = "plugins.devops",
+  },
 }
+
